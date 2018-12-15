18d637af83e085ee40ca1bd3dd2c69746ca57a63
Beatrix Klebe <bea@klebe.blog>
2018-12-15 18:37:30 -0500

    Autosave undo tree history

--- lisp/backup.el
+++ lisp/backup.el
@@ -1,6 +1,12 @@
+;; store all backup and autosave files in the tmp dir
 (setq backup-directory-alist
       `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
+
+;; autosave the undo-tree history
+(setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq undo-tree-auto-save-history t)
 
 (provide 'backup)

189b982063d4f461114a92c8c8a01d2b232d29e8
Beatrix Klebe <bea@klebe.blog>
2018-12-15 18:17:30 -0500

    Cleanup OCaml config in prep for major rearchitecture

--- lisp/ocaml.el
+++ lisp/ocaml.el
@@ -1,11 +1,7 @@
-; Make company aware of merlin
-(with-eval-after-load 'company
- (add-to-list 'company-backends 'merlin-company-backend))
-; Enable company on merlin managed buffers
-; (add-hook 'merlin-mode-hook 'company-mode)
-; Or enable it globally:
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-dabbrev-downcase 0)
-(setq company-idle-delay 0)
+(require 'tuareg)
+(require 'utop)
+(require 'merlin)
+
+(add-to-list 'company-backends 'merlin-company-backend)
 
 (provide 'ocaml)

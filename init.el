806758952e8399c11c72a6ee77f4583d64c34a5c
Beatrix Klebe <bea@klebe.blog>
2018-12-15 17:00:48 -0500

    Configure paths to be relative to root to increase cross-platform compatibility

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(setq custom-file "~/.emacs.d/custom.el")
-(if (file-exists-p "~/.emacs.d/custom.el")
-    (load custom-file))
+(defvar root-dir (file-name-directory load-file-name)
+  "The root of the current emacs configuration.")
+
+(defvar lisp-dir (expand-file-name "lisp" root-dir)
+  "Location of my custom lisp modules.")
+
+(defvar custom-storage-file (expand-file-name "custom.el" lisp-dir))
+
+(add-to-list 'load-path lisp-dir)
+(setq custom-file custom-storage-file)
+
+(if (file-exists-p custom-storage-file)
+    (load custom-storage-file))
 
 (require 'pkg-init)
 (require 'shell)

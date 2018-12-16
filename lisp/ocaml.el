077b691aec569401c18e2fa5d3bcb6cc202c03d4
Beatrix Klebe <bea@klebe.blog>
2018-12-15 20:03:55 -0500

    Fix OCaml configuration

--- lisp/ocaml.el
+++ lisp/ocaml.el
@@ -1,19 +1,18 @@
 (eval-and-compile (require 'use-package))
 
-(use-package merlin :defer t
+(use-package merlin
+  :hook (tuareg-mode . merlin-mode)
   :config
   (setq merlin-error-after-save nil)
   (add-to-list 'company-backends 'merlin-company-backend))
 
-(use-package utop :defer t)
+(use-package utop
+  :hook (tuareg-mode . utop-minor-mode))
 
-(use-package flycheck-ocaml :defer t
-  :after flycheck-mode
+(use-package flycheck-ocaml
   :config
   (flycheck-ocaml-setup))
 
-(use-package tuareg :defer t
-  :after (utop merlin flycheck-ocaml)
-  :hook (utop-minor-mode merlin-mode))
+(use-package tuareg :defer t)
 
 (provide 'ocaml)

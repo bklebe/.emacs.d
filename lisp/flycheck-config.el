e69ca0b3fc0cfe5ec48b1be293bbd6b67fa07aee
Beatrix Klebe <bea@klebe.blog>
2019-03-05 15:47:26 -0500

    Make flycheck load the lisp directory.

--- lisp/flycheck-config.el
+++ lisp/flycheck-config.el
@@ -1,8 +1,13 @@
 ;;; flycheck-config.el --- Settings for Emacs Flycheck
+
 ;;; Commentary:
+
 ;;; Code:
-(use-package flycheck
-  :config (global-flycheck-mode))
+
+(use-package flycheck :demand t
+  :config
+  (global-flycheck-mode)
+  (add-to-list 'flycheck-emacs-lisp-load-path lisp-dir))
 
 (provide 'flycheck-config)
 ;;; flycheck-config.el ends here

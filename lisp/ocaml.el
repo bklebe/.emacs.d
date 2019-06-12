7ccdf6fcb90fcbac5997488ae7f2b8683c57df91
Beatrix Klebe <bea@klebe.blog>
2019-06-11 20:35:44 -0400

    Cleaned up OCaml config

--- lisp/ocaml.el
+++ lisp/ocaml.el
@@ -4,6 +4,11 @@
 
 ;;; Code:
 
+(use-package tuareg
+  :bind ("C-c C-s" . utop)
+  :config
+  (setq compile-command "opam config exec corebuild "))
+
 (use-package merlin
   :hook (tuareg-mode . merlin-mode)
   :config
@@ -11,17 +16,12 @@
   (add-to-list 'company-backends 'merlin-company-backend)
   (flycheck-ocaml-setup))
 
+(use-package flycheck-ocaml)
+
 (use-package utop
   :hook (tuareg-mode . utop-minor-mode)
   :config
   (setq utop-command "opam config exec utop -- -emacs"))
-
-(use-package flycheck-ocaml)
-
-(use-package tuareg
-  :bind ("C-c C-s" . utop)
-  :config
-  (setq compile-command "opam config exec corebuild "))
 
 (use-package dune)
 

0f02c78c890f595ff6aaca54f0f410b7354d70f9
Beatrix Klebe <bea@klebe.blog>
2019-07-17 19:01:29 -0400

    Move package-config.el into init.el

--- init.el
+++ init.el
@@ -27,8 +27,50 @@
 
 (add-to-list 'load-path lisp-dir)
 
-;;; This must be called first to setup use-package
-(require 'package-config)
+;; Cribs liberally from bbatsov/prelude/core/prelude-packages.el
+(require 'cl)
+(require 'package)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(defvar bootstrap-packages
+  '(use-package
+    delight))
+
+(defun bootstrap-packages-installed-p ()
+  "Check if all packages in 'bootstrap-packages' are installed."
+  (every #'package-installed-p bootstrap-packages))
+
+(defun install-package (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun install-packages (packages)
+  (mapc #'install-package packages))
+
+(defun install-bootstrap-packages ()
+  "Install all packages required to bootstrap our setup."
+  (unless (bootstrap-packages-installed-p)
+    (package-refresh-contents)
+    (install-packages bootstrap-packages)))
+
+(install-bootstrap-packages)
+
+(eval-when-compile (require 'use-package))
+(require 'bind-key)
+
+(setq use-package-always-ensure t)
+(setq use-package-always-defer t)
+
+(use-package delight)
+
+(use-package auto-package-update
+  :config
+  (setq auto-package-update-delete-old-versions t)
+  (setq auto-package-update-hide-results t)
+  (auto-package-update-maybe))
 
 (require 'ui)
 (require 'backup)

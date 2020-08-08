d4342d161ffe0f5122980b6ef63558a7286d5887
Beatrix Klebe <bklebe@mbta.com>
2020-08-08 14:07:59 -0400

    Add dockerfile-mode and dash-at-point

--- init.el
+++ init.el
@@ -128,6 +128,10 @@
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2))
 
+(use-package dash-at-point)
+
+(use-package dockerfile-mode)
+
 (global-git-commit-mode t)
 
 (require 'org-mode-config)

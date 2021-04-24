ff5cf609869a7b2e49d0a2b17e7f98cf37e43664
Beatrix Klebe <beatrixk@hey.com>
2021-04-24 19:34:07 -0400

    Move development.el into init.el

--- init.el
+++ init.el
@@ -164,7 +164,44 @@
 
 (set-default 'truncate-lines t)
 
-(require 'development)
+(use-package company
+  :hook (prog-mode . company-mode)
+  :config
+  (setq company-idle-delay 0)
+  (setq company-dabbrev-downcase nil))
+
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+(use-package forge :demand t
+  :after magit)
+
+(use-package projectile
+  :bind (("s-p" . projectile-command-map)
+         ("C-c p" . projectile-command-map))
+  :config
+  (projectile-mode t))
+
+(use-package csv-mode)
+
+(use-package yasnippet
+  :hook (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package yasnippet-snippets)
+
+(use-package eglot
+  :hook (rust-mode . eglot-ensure))
+
+(require 'ocaml)
+(require 'dotnet)
+(require 'proof-general-config)
+(require 'rust)
+(require 'python-tools)
 
 (use-package git-commit)
 

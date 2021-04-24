5f997af5400f0bd8f56ecb1a0abdac4f38db762e
Beatrix Klebe <beatrixk@hey.com>
2021-04-24 19:27:38 -0400

    Move editor settings into init.el

--- init.el
+++ init.el
@@ -152,7 +152,17 @@
 ;; each 50MB of allocated data (the default is on every 0.76MB).
 (setq gc-cons-threshold 50000000)
 
-(require 'editor)
+
+;;; editor settings
+(setq require-final-newline t)
+(delete-selection-mode t)
+(setq-default indent-tabs-mode nil)   ;; don't use tabs to indent
+(setq-default tab-width 8)            ;; but maintain correct appearance
+(setq tab-always-indent 'complete)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq next-line-add-newlines nil)
+
+(set-default 'truncate-lines t)
 
 (require 'development)
 

a2e1d9f3c88973f359e6fbaabf2ec7b853a714e6
Beatrix Klebe <beatrixk@hey.com>
2021-08-26 18:08:27 -0400

    don't collect garbage at startup (rec from DOOM emacs)

--- init.el
+++ init.el
@@ -1,1 +1,9 @@
+(setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
+      gc-cons-percentage 0.6)
+
+(add-hook 'emacs-startup-hook
+  (lambda ()
+    (setq gc-cons-threshold 16777216 ; 16mb
+          gc-cons-percentage 0.1)))
+
 (org-babel-load-file "~/.emacs.d/README.org")

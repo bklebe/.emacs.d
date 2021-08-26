100a2b9d6d3e4ba03deb7d4cbacef65bed435f8d
Beatrix Klebe <beatrixk@hey.com>
2021-08-26 18:50:35 -0400

    speed adjustments

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
+(defvar file-name-handler-alist-cache file-name-handler-alist)
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
-      gc-cons-percentage 0.6)
-
+      gc-cons-percentage 0.6
+      file-name-handler-alist nil)
 (add-hook 'emacs-startup-hook
-  (lambda ()
-    (setq gc-cons-threshold 16777216 ; 16mb
-          gc-cons-percentage 0.1)))
+          (lambda ()
+            (setq gc-cons-threshold 16777216 ; 16mb
+                  gc-cons-percentage 0.1
+                  file-name-handler-alist file-name-handler-alist-cache)))
 
-(org-babel-load-file "~/.emacs.d/README.org")
+(org-babel-load-file "~/.emacs.d/README.org" t)

4b86435812a7e5e42b0a9644527544aaa3279e8e
Beatrix Klebe <beatrixk@hey.com>
2021-08-27 13:42:06 -0400

    add bytecomp hints

--- init.el
+++ init.el
@@ -1,4 +1,6 @@
-(defvar file-name-handler-alist-cache file-name-handler-alist)
+;;; -*- lexical-binding: t; -*-
+
+(defvar file-name-handler-alist-handle file-name-handler-alist)
 (setq gc-cons-threshold most-positive-fixnum ; 2^61 bytes
       gc-cons-percentage 0.6
       file-name-handler-alist nil)
@@ -6,6 +8,6 @@
           (lambda ()
             (setq gc-cons-threshold 16777216 ; 16mb
                   gc-cons-percentage 0.1
-                  file-name-handler-alist file-name-handler-alist-cache)))
+                  file-name-handler-alist file-name-handler-alist-handle)))
 
 (org-babel-load-file "~/.emacs.d/README.org" t)

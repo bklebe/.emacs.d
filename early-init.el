18c87687424c0b4c71e043682abd60b4ac25d90f
Beatrix Klebe <bklebe@mbta.com>
2022-11-30 14:51:04 -0500

    fix: move straight init into early-init.el

--- early-init.el
+++ early-init.el
@@ -1,3 +1,33 @@
 ;; -*- lexical-binding: t; -*-
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+(setq use-package-always-defer t)
+
+;;; org-mode configuration
+(use-package org
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c c" . org-capture)
+   ("C-c b" . org-switchb))
+  :config (setq org-export-backends '(md txt)
+                org-edit-src-content-indentation 0
+                org-src-preserve-indentation t
+                org-src-tab-acts-natively t))
+
 (org-babel-load-file "~/.emacs.d/early.org" t)
